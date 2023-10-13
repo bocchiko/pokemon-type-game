f608cc2d81bb61b6aabc3a97c05095b235079afd
bocchiko <luisbonilla0908@gmail.com>
2023-10-13 15:12:19 -0600

    Play State

---
+++ src/states/game/FieldMenuState.lua
@@ -0,0 +1,13 @@
+FieldMenuState = Class{__includes = BaseState}
+
+function FieldMenuState:init()
+    
+end
+
+function FieldMenuState:update(dt)
+    
+end
+
+function FieldMenuState:render()
+    
+end
